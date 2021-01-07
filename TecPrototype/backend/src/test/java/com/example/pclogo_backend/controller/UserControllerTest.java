b4985fd0657a34fe95e9b1ac449f19e9dfdbfbe4
coodlover <38162204+coodlover@users.noreply.github.com>
2021-01-07 17:06:36 +0800

    add user controller test case

--- TecPrototype/backend/src/test/java/com/example/pclogo_backend/controller/UserControllerTest.java
+++ TecPrototype/backend/src/test/java/com/example/pclogo_backend/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.example.pclogo_backend.controller;
 
 import com.example.pclogo_backend.constant.Constant;
 import com.example.pclogo_backend.entity.User;
+import com.example.pclogo_backend.utils.msgutils.Msg;
 import com.example.pclogo_backend.utils.msgutils.MsgCode;
 import com.example.pclogo_backend.utils.msgutils.MsgUtil;
 import org.junit.After;
@@ -22,10 +23,16 @@ class UserControllerTest {
     private UserController userController;
     @Test
     void login() {
+        //case 1 success
         Map<String,String> params = new HashMap<>();
-        params.put("name","Alice");
+        params.put("username","Alice");
         params.put("password","1234");
-        assertNotEquals(MsgUtil.makeMsg(MsgCode.LOGIN_USER_ERROR),userController.login(params));
+        assertEquals(MsgCode.SUCCESS.getStatus(),userController.login(params).getStatus());
+        //case 2 fail
+        Map<String,String> params2 = new HashMap<>();
+        params2.put("username","Alice");
+        params2.put("password","4321");
+        assertEquals(MsgCode.LOGIN_USER_ERROR.getStatus(),userController.login(params2).getStatus());
     }
 
     @Test
@@ -34,12 +41,32 @@ class UserControllerTest {
 
     @Test
     void addUser() {
+        //case 1 empty name
+        Map<String,String> param1 = new HashMap<>();
+        param1.put("name","");
+        param1.put("password","1234");
+        param1.put("phone","15811111111");
+        param1.put("email","Bob@sjtu.edu.cn");
+        Msg msg1 = userController.addUser(param1);
+        assertEquals(MsgCode.NOT_ENTERING_ANYTHING.getStatus(), msg1.getStatus());
+
+        //case 2 empty password
+        Map<String,String> param2 = new HashMap<>();
+        param2.put("name","Bob");
+        param2.put("password","");
+        param2.put("phone","15811111111");
+        param2.put("email","Bob@sjtu.edu.cn");
+        Msg msg2 = userController.addUser(param2);
+        assertEquals(MsgCode.NOT_ENTERING_ANYTHING.getStatus(), msg2.getStatus());
+
+        //case 3 success
         Map<String,String> param = new HashMap<>();
         param.put("name","Bob");
         param.put("password","1234");
         param.put("phone","15811111111");
         param.put("email","Bob@sjtu.edu.cn");
         assertNotEquals(MsgUtil.makeMsg(MsgCode.DUPLICATE_USER_ERROR), userController.addUser(param));
+
     }
 
     @Test
